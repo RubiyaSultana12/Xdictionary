f9480132c2f38c75534c0098526a124f8c00937d
RubiyaSultana12 <rubiya.sultana22@gmail.com>
2025-01-14 01:01:31 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [dictionary]= useState(
+  [
+  
+      { word: "React", meaning: "A JavaScript library for building user interfaces." },
+  
+      { word: "Component", meaning: "A reusable building block in React." },
+  
+      { word: "State", meaning: "An object that stores data for a component." }
+  
+  ])
+  const[searchTerm, setSearchTerm]=useState("");
+  const[result, setResult]=useState("");
+  
+  const handleClick=()=>{
+    const found=dictionary.find(
+      (item) => item.word.toLowerCase()=== searchTerm.toLowerCase()
+   
+    );
+
+    if(found){
+      setResult(found.meaning);
+    }
+    else{
+      setResult("Word not found in the dictionary")
+    }
+   
+  };
+  return (
+    <div className="App">
+     <h1>Dictionary App</h1>
+     <input type="text" placeholder='Search for a word...' value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)}/>
+     <button onClick={handleClick}>Search</button>
+     <div>
+     <strong>Definition:</strong>
+     <p>{result}</p>
+     </div>
+     
+    </div>
+  );
+}
+
+export default App;
