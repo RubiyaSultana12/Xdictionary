d3a6e70209ef5886493e314542466cb681075be5
RubiyaSultana <65206675+RubiyaSultana12@users.noreply.github.com>
2025-01-14 17:26:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React, { useState } from "react";
 import "./App.css";
 
-const dictionary=[
+const dictionary = [
   {
     word: "React",
     meaning: "A JavaScript library for building user interfaces.",
   },
-
   { word: "Component", meaning: "A reusable building block in React." },
-
   { word: "State", meaning: "An object that stores data for a component." },
-]
+];
+
 function App() {
- 
   const [search, setSearch] = useState("");
   const [result, setResult] = useState();
-  
- const handleSearch=e=>
-   setSearch(e.target.value);
-   
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
   const handleClick = () => {
-    let found = dictionary.find
-      (item => item.word.toLowerCase() === search.toLowerCase()
+    let found = dictionary.find(
+      (item) => item.word.toLowerCase() === search.toLowerCase()
     );
 
     if (found) {
@@ -30,10 +29,10 @@ function App() {
       setResult("Word not found in the dictionary");
     }
   };
+
   return (
     <div className="App">
       <h1>Dictionary App</h1>
-
       <input
         type="text"
         value={search}
