084d243c13b643de8968d8a9ad817ae91cd2c698
RubiyaSultana <65206675+RubiyaSultana12@users.noreply.github.com>
2025-01-14 23:02:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,37 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, { useState } from 'react';
 
 const dictionary = [
-  {
-    word: "React",
-    meaning: "A JavaScript library for building user interfaces.",
-  },
-  { word: "Component", meaning: "A reusable building block in React." },
-  { word: "State", meaning: "An object that stores data for a component." },
-];
+    { word: "React", meaning: "A JavaScript library for building user interfaces." },
+    { word: "Component", meaning: "A reusable building block in React." },
+    { word: "State", meaning: "An object that stores data for a component." }
+]
 
 function App() {
- const [text, setText] = useState("");
+    const [text, setText] = useState("");
     const [definition, setDefinition] = useState()
 
-  const handleSearch = (e) => {
-   setText(e.target.value)
-  };
+    const handleChange = e=> setText(e.target.value);
 
-  const handleClick = () => {
-    let found = dictionary.find(
-      (item) => item.word.toLowerCase() === text.toLowerCase()
-    );
-
-    if (found) {
-      setDefinition(found.meaning);
+    const handleClick = ()=> {
+        let item = dictionary.find(i=> i.word.toLocaleLowerCase() === text.toLocaleLowerCase());
+         if (item) {
+      setDefinition(item.meaning);
     } else {
       setDefinition("Word not found in the dictionary");
     }
-  };
+    }
 
-  return (
-    <div className="App">
-      <h1>Dictionary App</h1>
-      <input
-        type="text"
-        value={text}
-        placeholder="Search for a word..."
-        onChange={handleSearch}
-      />
-      <button onClick={handleClick}>Search</button>
-      <h4>Definition:</h4>
-       <p>{definition}</p>
-    </div>
-  );
-}
+    return (
+        <div>
+            <h1>Dictionary App</h1>
+            <input onChange={handleChange} type='text' placeholder='Search for a word...' />
+            <button onClick={handleClick}>Search</button>
+            <h4>Definition:</h4>
+            <p>{definition}</p>
+        </div>
+    );
+};
+
+
 
 export default App;
