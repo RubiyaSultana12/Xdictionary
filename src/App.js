7891a87f19bd8b0d23f34e1e6c8c1cd8deb807db
RubiyaSultana <65206675+RubiyaSultana12@users.noreply.github.com>
2025-01-14 09:07:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
-import { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
+const dictionary=[
+  {
+    word: "React",
+    meaning: "A JavaScript library for building user interfaces.",
+  },
+
+  { word: "Component", meaning: "A reusable building block in React." },
+
+  { word: "State", meaning: "An object that stores data for a component." },
+]
 function App() {
-  const [dictionary]= useState(
-  [
-  
-      { word: "React", meaning: "A JavaScript library for building user interfaces." },
-  
-      { word: "Component", meaning: "A reusable building block in React." },
-  
-      { word: "State", meaning: "An object that stores data for a component." }
-  
-  ])
-  const[searchTerm, setSearchTerm]=useState("");
-  const[result, setResult]=useState("");
-  
-  const handleClick=()=>{
-    const found=dictionary.find(
-      (item) => item.word.toLowerCase()=== searchTerm.toLowerCase()
-   
+ 
+  const [searchTerm, setSearchTerm] = useState("");
+  const [result, setResult] = useState();
+
+  const handleClick = () => {
+    const found = dictionary.find(
+      (item) => item.word.toLowerCase() === searchTerm.toLowerCase()
     );
 
-    if(found){
+    if (found) {
       setResult(found.meaning);
+    } else {
+      setResult("Word not found in the dictionary");
     }
-    else{
-      setResult("Word not found in the dictionary")
-    }
-   
   };
   return (
     <div className="App">
-     <h1>Dictionary App</h1>
-     <input type="text" placeholder='Search for a word...' value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)}/>
-     <button onClick={handleClick}>Search</button>
-     <div>
-     <strong>Definition:</strong>
-     <p>{result}</p>
-     </div>
-     
+      <h1>Dictionary App</h1>
+
+      <input
+        type="text"
+        placeholder="Search for a word..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button onClick={handleClick}>Search</button>
+      <h4>Definition:</h4>
+      <p>{result}</p>
     </div>
   );
 }
