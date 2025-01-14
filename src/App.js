ddce458629fa356034d3fa4b9bec96d35cd862b4
RubiyaSultana <65206675+RubiyaSultana12@users.noreply.github.com>
2025-01-14 10:10:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "./App.css";
 
-const dictionary = [
-    { word: "React", meaning: "A JavaScript library for building user interfaces." },
-    { word: "Component", meaning: "A reusable building block in React." },
-    { word: "State", meaning: "An object that stores data for a component." }
+const dictionary=[
+  {
+    word: "React",
+    meaning: "A JavaScript library for building user interfaces.",
+  },
+
+  { word: "Component", meaning: "A reusable building block in React." },
+
+  { word: "State", meaning: "An object that stores data for a component." },
 ]
+function App() {
+ 
+  const [searchTerm, setSearchTerm] = useState("");
+  const [result, setResult] = useState();
 
-const XDictionary = () => {
-    const [text, setText] = useState("");
-    const [definition, setDefinition] = useState()
+  const handleClick = () => {
 
-    const handleChange = e=> setText(e.target.value);
 
-    const handleClick = ()=> {
-        let item = dictionary.find(i=> i.word.toLocaleLowerCase() === text.toLocaleLowerCase());
-        if(item){  
-        setDefinition(item.meaning);
-    }
-        else{
-        setDefinition("Word not found in the dictionary.")
-        }
-    }
-
-    return (
-        <div>
-            <h1>Dictionary App</h1>
-            <input onChange={handleChange} type='text' placeholder='Search for a word...' />
-            <button onClick={handleClick}>Search</button>
-            <h4>Definition:</h4>
-            <p>{definition}</p>
-        </div>
+    const found = dictionary.find(
+      (item) => item.word.toLowerCase() === searchTerm.toLowerCase()
     );
-};
 
-export default XDictionary;
+    if (found) {
+      setResult(found.meaning);
+    } else {
+      setResult("Word not found in the dictionary");
+    }
+  };
+  return (
+    <div className="App">
+      <h1>Dictionary App</h1>
+
+      <input
+        type="text"
+        placeholder="Search for a word..."
+        value={searchTerm}
+        onChange={((e) => setSearchTerm(e.target.value))}
+      />
+      <button onClick={handleClick}>Search</button>
+      <h4>Definition:</h4>
+      <p>{result}</p>
+    </div>
+  );
+}
+
+export default App;
